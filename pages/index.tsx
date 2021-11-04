7d397762f6f78028fc3867d38be1e5386fa35c8e
polisen <29376308+polisen@users.noreply.github.com>
2021-11-04 13:43:51 +0100

    created super simple ui and progress bar experiment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,52 @@
-import styled from 'styled-components';
-import React, { useEffect } from 'react';
-import Dropzone from 'components/Dropzone';
+import React, { useEffect } from "react";
+import Dropzone from "components/Dropzone";
 // import { createFFmpeg } from '@ffmpeg/ffmpeg';
-import useBundler from 'hooks/useBundler';
-import { useSelector } from 'react-redux';
-import Files from 'components/Files';
+import styled from "styled-components";
+import useBundler from "hooks/useBundler";
+import { useSelector } from "react-redux";
+import Files from "components/Files";
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const DivDiv = styled.div`
+  #box-left-mini {
+    float: left;
+    width: 100%;
+  }
+
+  #box-left-mini #front {
+    display: flex;
+    height: 24px;
+    border: 1px solid #3b3b3b;
+    align-items: center;
+    justify-content: center;
+    border-radius: 8px;
+
+    z-index: 5;
+    position: relative;
+    p {margin: 0;}
+  }
+  #box-left-mini #front span {
+    /* background: #fff; */
+  }
+
+  #box-left-mini #behind_container {
+  background-color: #3b3b3b;
+    position: relative;
+    border-radius: 8px;
+    height: 24px;
+    width: 75%;
+    top: -24px;
+  }
+  #box-left-mini #behind {
+    display: flex;
+    z-index: 3;
+  }
 `;
 
 export default function Home() {
@@ -24,12 +60,26 @@ export default function Home() {
   }
 
   return (
-    <Container>
-      {
-        Object.keys(files).length > 0
-          ? <Files />
-          : <Dropzone setFiles={(acceptedFiles: File[]) => handleFiles(acceptedFiles)} />
-      }
-    </Container>
+    <>
+      <Container>
+        {Object.keys(files).length > 0 ? (
+          <Files />
+        ) : (
+          <Dropzone
+            setFiles={(acceptedFiles: File[]) => handleFiles(acceptedFiles)}
+          />
+        )}
+      </Container>
+      <DivDiv>
+        <div id="box-left-mini">
+          <div id="front">
+            <span>this is in front</span>
+          </div>
+          <div id="behind_container">
+            {/* <div id="behind">behind</div> */}
+          </div>
+        </div>
+      </DivDiv>
+    </>
   );
 }
